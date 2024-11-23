4a2214d04bf30a0360698778aaecb374ff602d34
artem <huaweitit0101@gmail.com>
2024-11-23 19:41:35 +0300

    docs

--- engine.py
+++ engine.py
@@ -2,6 +2,6 @@ import sqlalchemy
 login = input()
 password = input()
 engine = sqlalchemy.create_engine(f"mysql+pymysql://{login}:{password}@localhost:3306/recipes")
-sess = sqlalchemy.orm.sessionmaker(bind=engine)
+session = sqlalchemy.orm.sessionmaker(bind=engine)
 class Base(sqlalchemy.orm.DeclarativeBase):
     pass
