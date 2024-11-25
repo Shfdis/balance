baab39449d0bf9231f697f667a528281a8dee892
Shfdis <94714022+Shfdis@users.noreply.github.com>
2024-11-25 22:42:05 +0300

    Delete engine.py

--- engine.py
+++
@@ -1,7 +0,0 @@
-import sqlalchemy
-login = input()
-password = input()
-engine = sqlalchemy.create_engine(f"mysql+pymysql://{login}:{password}@localhost:3306/recipes")
-session = sqlalchemy.orm.sessionmaker(bind=engine)
-class Base(sqlalchemy.orm.DeclarativeBase):
-    pass
