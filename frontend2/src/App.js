02117b61b57626dd36bd29990bc01672a9010b74
artem <huaweitit0101@gmail.com>
2025-03-07 17:38:02 +0300

    fixed app.js

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/loginPage';
 import Home from './Pages/Home';
 import Coef from './Pages/Coef';
+const APIUrl = "http://127.0.0.1:5000"
 function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/coef" element={<Coef />} />
+        <Route path="/login" element={<LoginPage APIUrl={APIUrl}/>} />
+        <Route path="/home" element={<Home APIUrl={APIUrl}/>} APIUrl=""/>
+        <Route path="/coef" element={<Coef APIUrl={APIUrl}/>} APIUrl=""/>
       </Routes>
     </BrowserRouter>
   )
