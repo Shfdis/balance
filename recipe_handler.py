ff7d931bc5813014bb757f63ebf490551ad14791
Vladimir <yyaakkoovlev@gmail.com>
2025-03-13 15:14:52 +0300

    пофиксил сабмит

--- recipe_handler.py
+++ recipe_handler.py
@@ -1,6 +1,10 @@
+from sqlalchemy.orm import Session
+
 from db.db_utils.db_session import create_session
 from db.db_utils.__all_models import *
 import json
+
+
 class RecipeHandler:
     def __init__(self, usersRecipe: RecipeUser, recipe: Recipe):
         """
@@ -24,7 +28,8 @@ class RecipeHandler:
             for i in ingridient["tastes"]:
                 map[ingridient["name"]].update({i["name"]: i["value"]})
         self.changeCoefficients = map
-    def alter_recipe(self, deltas: dict):
+
+    def alter_recipe(self, deltas: dict, session: Session):
         """
         Alters the recipe's measures based on the provided deltas and updates the database.
 
@@ -36,30 +41,27 @@ class RecipeHandler:
         Args:
             deltas (dict): A dictionary containing the properties and their delta changes.
         """
-        with create_session() as session:
-            with session.begin():
-                # Create a new measures dictionary by copying the current measures
-                newMeasures = self.measures.copy()
+        # Create a new measures dictionary by copying the current measures
+        newMeasures = self.measures.copy()
 
-                # Iterate through the coeficients of the recipe table
-                for (ingridient, value) in self.measures.items():
-                    # Iterate through the taste and coefficient pairs
-                    for taste, coef in self.changeCoefficients[ingridient].items():
-                        # Adjust the measures of the ingridient by the given delta
-                        newMeasures[ingridient][0] += coef * value[0] * self.coef * deltas[taste]
+        # Iterate through the coeficients of the recipe table
+        for (ingridient, value) in self.measures.items():
+            # Iterate through the taste and coefficient pairs
+            if ingridient in self.changeCoefficients:
+                for taste, coef in self.changeCoefficients[ingridient].items():
+                    # Adjust the measures of the ingridient by the given delta
+                    newMeasures[ingridient][0] += coef * value[0] * self.coef * deltas[taste]
 
-                # Update the measures and apply changes to the database
-                self.measures = []
-                for (name, (value, id)) in newMeasures.items():
-                    self.measures.append({"name": name, "value": value, "id": id})
+        # Update the measures and apply changes to the database
+        self.measures = []
+        for (name, (value, id)) in newMeasures.items():
+            self.measures.append({"name": name, "value": value, "id": id})
 
-                # Adjust the coefficient for the recipe
-                self.coef *= 0.9
-                self.coef = max(0.05, self.coef)
+        # Adjust the coefficient for the recipe
+        self.coef *= 0.9
+        self.coef = max(0.05, self.coef)
 
-                # Update the coefficient in the user's recipe record
-                recipe = session.query(RecipeUser).where(RecipeUser.id == self.id).first()
-                recipe.recipe_json_data = json.dumps(self.measures)
-                recipe.change_coef = self.coef
-                # Commit the transaction
-                session.commit()
+        # Update the coefficient in the user's recipe record
+        recipe = session.query(RecipeUser).where(RecipeUser.id == self.id).first()
+        recipe.recipe_json_data = json.dumps(self.measures)
+        recipe.change_coef = self.coef
